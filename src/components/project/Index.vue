604104ffaba519ef259fe4a03e360c3abdbd6c35
songhongtu <songhongtu@lyarc.com>
2019-06-17 09:20:53 +0800

    hzy

--- src/components/project/Index.vue
+++ src/components/project/Index.vue
@@ -27,6 +27,8 @@
           <td>
             <Button @click="reset" type="default">重置</Button>
           </td>
+          <td>&nbsp;</td>
+          <td><Button @click="exportDown" type="info" icon="ios-download-outline">导出</Button></td>
         </tr>
       </table>
     </div>
@@ -240,6 +242,9 @@ export default {
           }
         });
       }
+    },
+    exportDown(){
+      this.$refs.page.exportDown();
     },
     goBack: function () {
       this.$router.go(-1);
